a206fca20e40c09c8e3e4550076e781a9dde125e
Saroja Bamra <sarojabamra@gmail.com>
2024-05-28 22:21:34 +0530

    add frontend

---
+++ client/src/components/compose/Edit.jsx
@@ -0,0 +1,121 @@
+import React, { useState, useContext } from "react";
+import "./Compose.css";
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { DataContext } from "../../context/DataProvider";
+import Dropdown from "../dropdown/Dropdown";
+import TagInput from "../tagInput/TagInput";
+import { API } from "../../service/api";
+
+const initialPost = {
+  title: "",
+  description: "",
+  username: "",
+  name: "",
+  category: "",
+  tags: [],
+  createdDate: new Date(),
+};
+
+const Edit = () => {
+  const { id } = useParams();
+  const [post, setPost] = useState(initialPost);
+
+  const { account } = useContext(DataContext);
+
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setPost((prevPost) => ({
+      ...prevPost,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  useEffect(() => {
+    const fetchData = async () => {
+      let response = await API.getPostById(id);
+      if (response.isSuccess) {
+        setPost(response.data);
+      }
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    post.username = account.username;
+    post.name = account.name;
+  }, [account, post]);
+
+  useEffect(() => {
+    setPost((prevPost) => ({
+      ...prevPost,
+      category: selectedCategory,
+    }));
+  }, [selectedCategory]);
+
+  const setTags = (newTags) => {
+    setPost((prevPost) => ({
+      ...prevPost,
+      tags: newTags,
+    }));
+  };
+
+  const updatePost = async () => {
+    if (!post.title || !post.description || !post.category) {
+      alert("Title, Description, and Category are required.");
+      return;
+    }
+    let response = await API.updatePost(post);
+
+    if (response.isSuccess) {
+      navigate(`/details/${id}`);
+    }
+  };
+
+  return (
+    <>
+      <div className="compose">
+        <div className="box">
+          <div className="cols">
+            <div className="col1">
+              <input
+                className="inputtitle"
+                placeholder="Give your Blog a Title..."
+                name="title"
+                value={post.title}
+                onChange={(e) => handleChange(e)}
+              />
+              <p className="user">{account.username}</p>
+              <div>
+                <TagInput tags={post.tags} setTags={setTags} />
+              </div>
+            </div>
+            <div className="btns">
+              <div className="categorylist">
+                <Dropdown
+                  selected={post.category}
+                  setSelected={setSelectedCategory}
+                />
+              </div>
+              <div className="publish">
+                <button onClick={() => updatePost()}>Update</button>
+              </div>
+            </div>
+          </div>
+          <div className="text">
+            <div className="hr" />
+            <textarea
+              placeholder="Tell your story..."
+              value={post.description}
+              name="description"
+              onChange={(e) => handleChange(e)}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Edit;
---
+++ client/src/components/login/Login.jsx
@@ -0,0 +1,156 @@
+import React, { useState, useContext } from "react";
+import "./Login.css";
+import { API } from "../../service/api";
+import { DataContext } from "../../context/DataProvider";
+import { useNavigate } from "react-router-dom";
+
+const signupInitialValues = {
+  name: "",
+  username: "",
+  password: "",
+};
+
+const loginInitialValues = {
+  username: "",
+  password: "",
+};
+
+const Login = ({ isUserAuthenticated }) => {
+  const [loginPage, setLoginPage] = useState("signin");
+  const [signup, setSignup] = useState(signupInitialValues);
+  const [error, setError] = useState("");
+  const [signin, setSignin] = useState(loginInitialValues);
+
+  const { setAccount } = useContext(DataContext);
+  const navigate = useNavigate();
+
+  const toggleSignUp = () => {
+    setLoginPage("signup");
+  };
+
+  const toggleSignIn = () => {
+    setLoginPage("signin");
+  };
+
+  const onInputChange = (e) => {
+    setSignup({ ...signup, [e.target.name]: e.target.value });
+  };
+
+  const onValueChange = (e) => {
+    setSignin({ ...signin, [e.target.name]: e.target.value });
+  };
+
+  const signupUser = async () => {
+    let response = await API.userSignup(signup);
+    if (response.isSuccess) {
+      setError("");
+      setSignup(signupInitialValues);
+      setLoginPage("signin");
+    } else {
+      setError("Something went wrong. Please try again later.");
+    }
+  };
+
+  const signinUser = async () => {
+    let response = await API.userSignin(signin);
+
+    if (response.isSuccess) {
+      setError("");
+
+      sessionStorage.setItem(
+        "accessToken",
+        `Bearer ${response.data.accessToken}`
+      );
+      sessionStorage.setItem(
+        "refreshToken",
+        `Bearer ${response.data.refreshToken}`
+      );
+
+      setAccount({
+        username: response.data.username,
+        name: response.data.name,
+      });
+
+      isUserAuthenticated(true);
+
+      navigate("/");
+    } else {
+      setError("Something went wrong. Please try again later.");
+    }
+  };
+
+  return (
+    <>
+      <div className="bg">
+        {loginPage === "signin" ? (
+          <div className="container">
+            <div className="signin">
+              <h1>Sign In</h1>
+
+              <input
+                placeholder="Enter Username"
+                value={signin.username}
+                type="text"
+                onChange={(e) => onValueChange(e)}
+                name="username"
+              />
+
+              <input
+                placeholder="Enter Password"
+                value={signin.password}
+                type="password"
+                onChange={(e) => onValueChange(e)}
+                name="password"
+              />
+              {error && <p className="error">{error}</p>}
+              <button onClick={() => signinUser()}>Sign In</button>
+              <div className="toggle">
+                <p>
+                  Don't have an Account?{" "}
+                  <span onClick={() => toggleSignUp()}>Sign Up</span>
+                </p>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div className="container">
+            <div className="signup">
+              <h1>Sign Up</h1>
+
+              <input
+                placeholder="Enter Name"
+                type="text"
+                onChange={(e) => onInputChange(e)}
+                name="name"
+              />
+
+              <input
+                placeholder="Enter Username"
+                type="text"
+                onChange={(e) => onInputChange(e)}
+                name="username"
+              />
+
+              <input
+                placeholder="Enter Password"
+                type="password"
+                onChange={(e) => onInputChange(e)}
+                name="password"
+              />
+              {error && <p className="error">{error}</p>}
+              <button onClick={() => signupUser()}>Sign Up</button>
+              <div className="toggle">
+                <p>
+                  Already have an Account?{" "}
+                  <span onClick={() => toggleSignIn()}>Sign In</span>
+                </p>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Login;
